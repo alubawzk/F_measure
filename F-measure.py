102ca4cc0eb1894601e84fd5540ed6887d7c8379
alubawzk <ricardo_wzk@outlook.com>
2019-12-11 08:42:59 +0800

    更新了数据库和单张图片的选择

--- F-measure.py
+++ F-measure.py
@@ -11,7 +11,7 @@ DBL_MAX = 1.7976931348623158e+308
 RELATIVE_ERROR_FACTOR = 100.0
 DBL_EPSILON = 2.2204460492503131e-016
 SQRT2 = 1.41421356237
-TH_SCORE = 0.95
+TH_SCORE = 0.85
 outfile="binaries.txt"
 wildcard = ".txt .jpg .pgm .png"
 thick_type = -1
@@ -159,8 +159,8 @@ def rosin_point(ell_out, x, y):
 def Evaluation(th_score, img, filepath, image_name):
     ## img原图像
     size = img.shape
-    sy = size[0] # height
-    sx = size[1] # width
+    sx = size[0] # height
+    sy = size[1] # width
     filename = filepath + 'ground_truth/' + image_name + '.txt'
     if not is_txt_file(filename):
         raise Exception("%s 不是一个txt文件" % filename)
@@ -194,11 +194,9 @@ def Evaluation(th_score, img, filepath, image_name):
         img_gt_i = np.full((sx,sy),0, dtype=np.uint8)
         cv.ellipse(img_gt_i, (int(cx[i]), int(cy[i])), (int(semi_a[i]), int(semi_b[i])), sita[i]*(180/math.pi), 0, 360, 255, thick_type)
         img_gt.append(img_gt_i)
-    cv.imshow('img_gt_i', img_gt_i)
-        #sz_gt += 1
-    
+    cv.imshow('img_gt_i', img_gt[4])
     ## 创建全0背景下的detected
-    filename = filepath + 'detected/out_ellipse.txt'
+    filename = filepath + 'method_origin/' + image_name + '.txt'
     if not is_txt_file(filename):
         raise Exception("%s 不是一个txt文件" % filename)
     pass
@@ -249,9 +247,6 @@ def Evaluation(th_score, img, filepath, image_name):
         sita = np.array(sita)
         ang_start = np.array(ang_start)
         ang_end = np.array(ang_end)
-        full = np.array(full)
-    # sz_test = 0
-    
     for i in range(len(label)):
         img_detct_i = np.full((sx,sy), 0, dtype=np.uint8)
         # img_gt.append(img_gt_i)
@@ -273,17 +268,17 @@ def Evaluation(th_score, img, filepath, image_name):
         else:
             img_detct_i = draw_F_C(img_detct_i, ell_out)
         img_detect.append(img_detct_i)
-    cv.imshow("img_detct_i", img_detect[5])
+    # cv.imshow("img_detct_i", img_detect[5])
     size_test = len(img_detect)
-    sz_test = min(1000, size_test)
+    # sz_test = min(1000, size_test)
+    sz_test = size_test
     overlap = np.full((sz_gt,sz_test),0)
     for r in range(sz_gt):
         for c in range(sz_test):
-            if TestOverlap(img_gt[r], img_detect[c], th_score):
+            if TestOverlap(img_gt[r], img_detect[c], th_score, r, c):
                 overlap[r, c] = 255
             else:
                 overlap[r, c] = 0
-    # counter = 0
     vec_gt = [False for x in range(0,sz_gt)]
     for i in range(sz_test):
         for j in range(sz_gt):
@@ -312,9 +307,162 @@ def Evaluation(th_score, img, filepath, image_name):
         pr = tp / (tp+fp)
         re = tp / (tp+fn)
         fmeasure = (2*pr*re) / (pr+re)
-    return fmeasure
+    return fmeasure, re, pr
     
     
+def Evaluation_p(th_score, img, filepath, image_name):
+    ## img原图像
+    size = img.shape
+    sx = size[0] # height
+    sy = size[1] # width
+    filename = filepath + 'ground_truth/' + image_name + '.txt'
+    if not is_txt_file(filename):
+        raise Exception("%s 不是一个txt文件" % filename)
+    cx = []
+    cy = []
+    semi_b = []
+    semi_a = []
+    sita = []
+    img_gt = []
+    with open(filename, 'r') as file_to_read:
+        while True:
+            lines = file_to_read.readline()
+            if not lines:
+                break
+            pass
+            x, y, b, a, s = [float(i) for i in lines.split(',')]
+            cx.append(x)
+            cy.append(y)
+            semi_b.append(b)
+            semi_a.append(a)
+            sita.append(s)
+        pass
+        cx = np.array(cx)
+        cy = np.array(cy)
+        semi_b = np.array(semi_b)
+        semi_a = np.array(semi_a)
+        sita = np.array(sita)
+    sz_gt = len(cx)
+    for i in range(len(cx)):
+        # 创建全0的图
+        img_gt_i = np.full((sx,sy),0, dtype=np.uint8)
+        cv.ellipse(img_gt_i, (int(cx[i]), int(cy[i])), (int(semi_a[i]), int(semi_b[i])), sita[i]*(180/math.pi), 0, 360, 255, thick_type)
+        img_gt.append(img_gt_i)
+    # cv.imshow('img_gt_i', img_gt_i)
+    ## 创建全0背景下的detected
+    filename = filepath + 'method_propossed/' + image_name + '.txt'
+    if not is_txt_file(filename):
+        raise Exception("%s 不是一个txt文件" % filename)
+    pass
+    label = []
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    cx = []
+    cy = []
+    semi_b = []
+    semi_a = []
+    sita = []
+    ang_start = []
+    ang_end = []
+    full = []
+    img_detect = []
+    with open(filename, 'r') as file_to_read:
+        while True:
+            lines = file_to_read.readline()
+            if not lines:
+                break
+            pass
+            labell, xx1, yy1, xx2, yy2, x, y, ax, bx, theta, ang_s, ang_e, full_ = [
+                float(i) for i in lines.split()]
+            label.append(labell)
+            x1.append(xx1)
+            y1.append(yy1)
+            x2.append(xx2)
+            y2.append(yy2)
+            cx.append(x)
+            cy.append(y)
+            semi_a.append(ax)
+            semi_b.append(bx)
+            sita.append(theta)
+            ang_start.append(ang_s)
+            ang_end.append(ang_e)
+            full.append(full_)
+        label = np.array(label)
+        x1 = np.array(x1)
+        y1 = np.array(y1)
+        x2 = np.array(x2)
+        y2 = np.array(y2)
+        cx = np.array(cx)
+        cy = np.array(cy)
+        semi_a = np.array(semi_a)
+        semi_b = np.array(semi_b)
+        sita = np.array(sita)
+        ang_start = np.array(ang_start)
+        ang_end = np.array(ang_end)
+    for i in range(len(label)):
+        img_detct_i = np.full((sx,sy), 0, dtype=np.uint8)
+        # img_gt.append(img_gt_i)
+        ell_out = []
+        ell_out.append(x1[i])
+        ell_out.append(y1[i])
+        ell_out.append(x2[i])
+        ell_out.append(y2[i])
+        ell_out.append(cx[i])
+        ell_out.append(cy[i])
+        ell_out.append(semi_b[i])
+        ell_out.append(semi_a[i])
+        ell_out.append(sita[i])
+        ell_out.append(ang_start[i])
+        ell_out.append(ang_end[i])
+        ell_out.append(full[i])
+        if double_equal(semi_a[i], semi_b[i]):
+            img_detct_i = draw_F_E(img_detct_i, ell_out)
+        else:
+            img_detct_i = draw_F_C(img_detct_i, ell_out)
+        img_detect.append(img_detct_i)
+    # cv.imshow("img_detct_i", img_detect[5])
+    size_test = len(img_detect)
+    # sz_test = min(1000, size_test)
+    sz_test = size_test
+    overlap = np.full((sz_gt,sz_test),0)
+    for r in range(sz_gt):
+        for c in range(sz_test):
+            if TestOverlap(img_gt[r], img_detect[c], th_score, r, c):
+                overlap[r, c] = 255
+            else:
+                overlap[r, c] = 0
+    vec_gt = [False for x in range(0,sz_gt)]
+    for i in range(sz_test):
+        for j in range(sz_gt):
+            if vec_gt[j]:
+                continue
+            bTest = (overlap[j,i] != 0)
+            if bTest:
+                vec_gt[j] = True
+                break
+    tp = Count(vec_gt)
+    fn = int(sz_gt) - tp
+    fp = size_test - tp
+    pr = 0
+    re = 0
+    fmeasure = 0
+    if tp == 0:
+        if fp == 0:
+            pr = 1
+            re = 0
+            fmeasure = (2 * pr * re) / (pr + re)
+        else:
+            pr = 0
+            re = 0
+            fmeasure = 0
+    else:
+        pr = tp / (tp+fp)
+        re = tp / (tp+fn)
+        fmeasure = (2*pr*re) / (pr+re)
+    return fmeasure, re, pr
+
 
 def Count( v ):
     counter = 0
@@ -325,17 +473,10 @@ def Count( v ):
 
 
 
-def TestOverlap(gt, test, th):
-    fAND = cv.countNonZero(cv.add(gt, test))
-    fOR = cv.absdiff(gt, test)
-    fOR = cv.countNonZero(fOR)
-    if fAND == 0:
-        return False
-    # fOR  = cv.countNonZero(cv.subtract(test, gt))
-    # fAND = cv.countNonZero(gt + test)
-    # fOR  = cv.countNonZero(gt - test)
-    # fsim = fAND / fOR
-    fsim = fOR / fAND
+def TestOverlap(gt, test, th, r, c):
+    fOR = cv.countNonZero(cv.bitwise_or(gt, test)) # 并集
+    fAND = cv.countNonZero(cv.bitwise_and(gt, test)) #交集
+    fsim = fAND / fOR
     return fsim >= th
 
 
@@ -504,7 +645,7 @@ def draw_ellipse_arc(img, ell_out):
         x_2 = x2
         y_2 = y2
         if ((double_equal(x_1, x_2) and double_equal(y_1, y_2))
-                or dis(x_1, y_1, x_2, y_2) < 2.0):
+                or dis(x_1, y_1, x_2, y_2) < 3.0):
             cv.ellipse(img, (int(cx), int(cy)), (int(semi_a), int(semi_b)),
                        sita*(180/math.pi), 0, 360, (0, 255, 0))
             return
@@ -594,6 +735,80 @@ def show_detected_Ellipses(img, filepath, img_name):
             draw_ellipse_arc(img, ell_out)
 
 
+def show_detected_Ellipses_propossed(img, filepath, img_name):
+    filename = filepath + 'method_propossed/'+img_name
+    if not is_txt_file(filename):
+        raise Exception("%s 不是一个txt文件" % filename)
+    pass
+    label = []
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    cx = []
+    cy = []
+    semi_b = []
+    semi_a = []
+    sita = []
+    ang_start = []
+    ang_end = []
+    # ell_out = []
+    full = []
+    with open(filename, 'r') as file_to_read:
+        while True:
+            lines = file_to_read.readline()
+            if not lines:
+                break
+            pass
+            labell, xx1, yy1, xx2, yy2, x, y, ax, bx, theta, ang_s, ang_e, full_ = [
+                float(i) for i in lines.split()]
+            label.append(labell)
+            x1.append(xx1)
+            y1.append(yy1)
+            x2.append(xx2)
+            y2.append(yy2)
+            cx.append(x)
+            cy.append(y)
+            semi_a.append(ax)
+            semi_b.append(bx)
+            sita.append(theta)
+            ang_start.append(ang_s)
+            ang_end.append(ang_e)
+            full.append(full_)
+        label = np.array(label)
+        x1 = np.array(x1)
+        y1 = np.array(y1)
+        x2 = np.array(x2)
+        y2 = np.array(y2)
+        cx = np.array(cx)
+        cy = np.array(cy)
+        semi_a = np.array(semi_a)
+        semi_b = np.array(semi_b)
+        sita = np.array(sita)
+        ang_start = np.array(ang_start)
+        ang_end = np.array(ang_end)
+        full = np.array(full)
+    for i in range(len(label)):
+        ell_out = []
+        ell_out.append(x1[i])
+        ell_out.append(y1[i])
+        ell_out.append(x2[i])
+        ell_out.append(y2[i])
+        ell_out.append(cx[i])
+        ell_out.append(cy[i])
+        ell_out.append(semi_b[i])
+        ell_out.append(semi_a[i])
+        ell_out.append(sita[i])
+        ell_out.append(ang_start[i])
+        ell_out.append(ang_end[i])
+        ell_out.append(full[i])
+        if double_equal(semi_a[i], semi_b[i]):
+            draw_circ_arc(img, ell_out)
+        else:
+            draw_ellipse_arc(img, ell_out)
+
+
+
 def show_ground_truth(img, filepath, image_name):
     filename = filepath + 'ground_truth/' + image_name 
     cx = []
@@ -630,39 +845,79 @@ if __name__ == "__main__":
     # dir = 'images/'
     # file = open(outfile,"w")
     # file_name = ListFilesToTxt(dir,file,wildcard, 1)
-    f_str = []
-    file_dir = filepath + 'images/'
-    f = open('f_measure_origin/f_measure_origin.txt',"w")
-    for root, dirs, files in os.walk(file_dir):   
-        # img_name = 'ring4img4'
-        pass
-    for i in range(len(files)):
-        img_name = files[i]
+    filepath = ''
+    ####    数据集
+    file_dir = 'images'
+    for root, dirs, files in os.walk(file_dir):
+        img_name_ = files
+    for i in range(len(img_name_)):
+        img_name = img_name_[i]
+        detected_file = img_name.replace('.pgm','') + '.txt'
+        # img_name = 'ring4img3.pgm'
         detected_file = img_name.replace('.pgm','') + '.txt'
         gt_file = img_name.replace('.pgm','') + '.txt'
-        img_file = root + img_name
-        # img_file = filepath + 'images/' + img_name + '.pgm'
+        # img_file = root + img_name
+        img_file = filepath + 'images/' + img_name
         img = cv.imread(img_file)
         img_gt = img.copy()
         img_detected = img.copy()
+        img_detected_propossed = img.copy()
         size = img_gt.shape
         # 创建窗口
         cv.namedWindow('img_gt', cv.WINDOW_AUTOSIZE)
         cv.namedWindow('img_detected', cv.WINDOW_AUTOSIZE)
+        cv.namedWindow('img_detected_propossed', cv.WINDOW_AUTOSIZE)
         # 加载ground_truth & detected_ellipses
         show_ground_truth(img_gt, filepath, gt_file)
         show_detected_Ellipses(img_detected, filepath, detected_file)
+        show_detected_Ellipses_propossed(img_detected_propossed, filepath, detected_file)
         ## 计算指标
-        img_gry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        f_measure = Evaluation( TH_SCORE, img_gt, filepath, img_name.replace('.pgm','') )
-        f_str.append(f_measure)
-    with open('f_measure_origin/f_measure_origin.txt',"w") as f:    #设置文件对象
-        # str = f.read()    #可以是随便对文件的操作
-        f.write(f_str)
-
-    pass
+        f_score_o, re_o, pr_o = Evaluation( TH_SCORE, img_gt, filepath, img_name.replace('.pgm','') )
+        f_score_p, re_p, pr_p = Evaluation_p( TH_SCORE, img_gt, filepath, img_name.replace('.pgm','') )
+        f_score_out = ('f_measure_origin/'+img_name.replace('.pgm','')+'.txt')
+        # with open('f_measure_origin/f_measure.txt','w') as f:
+        with open(f_score_out,'w') as f:
+            f.write('img_name = {0}\n'.format(img_name))
+            f.write('f_score_o = {0}\tre_o = {1}\tpr_o = {2}\n'.format(f_score_o, re_o,pr_o))
+            f.write('f_score_p = {0}\tre_p = {1}\tpr_p = {2}\n'.format(f_score_p, re_p,pr_p))
+    print('f_score_o = {0}\tre_o = {1}\tpr_o = {2}\n'.format(f_score_o, re_o, pr_o))
+    print('f_score_p = {0}\tre_p = {1}\tpr_p = {2}\n'.format(f_score_p, re_p, pr_p))
+    ####    单张图片
+    """
+    img_name = 'ring4img3.pgm'
+    detected_file = img_name.replace('.pgm','') + '.txt'
+    gt_file = img_name.replace('.pgm','') + '.txt'
+    # img_file = root + img_name
+    
+    img_file = filepath + 'images/' + img_name
+    img = cv.imread(img_file)
+    img_gt = img.copy()
+    img_detected = img.copy()
+    img_detected_propossed = img.copy()
+    size = img_gt.shape
+    # 创建窗口
+    cv.namedWindow('img_gt', cv.WINDOW_AUTOSIZE)
+    cv.namedWindow('img_detected', cv.WINDOW_AUTOSIZE)
+    cv.namedWindow('img_detected_propossed', cv.WINDOW_AUTOSIZE)
+    # 加载ground_truth & detected_ellipses
+    show_ground_truth(img_gt, filepath, gt_file)
+    show_detected_Ellipses(img_detected, filepath, detected_file)
+    show_detected_Ellipses_propossed(img_detected_propossed, filepath, detected_file)
+    ## 计算指标
+    f_score_o, re_o, pr_o = Evaluation( TH_SCORE, img_gt, filepath, img_name.replace('.pgm','') )
+    f_score_p, re_p, pr_p = Evaluation_p( TH_SCORE, img_gt, filepath, img_name.replace('.pgm','') )
+    print('f_score_o = {0}\tre_o = {1}\tpr_o = {2}\n'.format(f_score_o, re_o, pr_o))
+    print('f_score_p = {0}\tre_p = {1}\tpr_p = {2}\n'.format(f_score_p, re_p, pr_p))
+    with open('f_measure_origin/f_measure.txt','w') as f:
+        f.write('img_name = {0}\n'.format(img_name))
+        f.write('f_score_o = {0}\tre_o = {1}\tpr_o = {2}\n'.format(f_score_o, re_o,pr_o))
+        f.write('f_score_p = {0}\tre_p = {1}\tpr_p = {2}\n'.format(f_score_p, re_p,pr_p))
+    """
     # show
+    cv.imwrite("img_gt.jpg", img_gt)
+    cv.imwrite("img_detected.jpg", img_detected)
+    cv.imwrite("img_detected_propossed.jpg", img_detected_propossed)
     cv.imshow('img_gt', img_gt)
     cv.imshow('img_detected', img_detected)
+    cv.imshow("img_detected_propossed", img_detected_propossed)
     cv.waitKey(0)
-    pass
